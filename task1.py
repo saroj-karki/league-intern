e810a03897f12ab1d444ff7df29d497e1bcba438
Saroj Karki <karki.saroj28@gmail.com>
2020-10-13 12:19:19 +0545

    second commit

--- task1.py
+++ task1.py
@@ -1,11 +1,15 @@
 import cv2
 
-img = cv2.imread("task1.png")
-hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-# cv2.imshow("output", img)
-low_value = (68, 72, 68)
-high_value = (68, 72, 68)
+def main():
+	img = cv2.imread("task1.png")
+	hsv_img = cv2.cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-imgfinal = cv2.inRange(hsv_img, low_value, high_value)
-cv2.imshow("output", imgfinal)
-cv2.waitKey(0)
+	low_value = (68, 72, 68)
+	high_value = (68, 72, 68)
+
+	img_final = cv2.inRange(hsv_img, low_value, high_value)
+	cv2.imshow("output", img_final)
+	cv2.waitKey(0)
+
+if __name__ == '__main__':
+	main()
