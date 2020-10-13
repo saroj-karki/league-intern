c13e317852646c1ac067b6254c8cec10547515c5
Saroj Karki <karki.saroj28@gmail.com>
2020-10-13 12:07:54 +0545

    second commit

--- task1.py
+++ task1.py
@@ -2,6 +2,10 @@ import cv2
 
 img = cv2.imread("task1.png")
 
-cv2.imshow("output", img)
-cv2.inRange(68, 72, 68)
+# cv2.imshow("output", img)
+low_value = (68, 72, 68)
+high_value = (68, 72, 68)
+
+imgfinal = cv2.inRange(low_value, high_value)
+cv2.imshow("output", imgfinal)
 cv2.waitKey(0)
