81fe1120a284b7bdd2ca2eebb9aa55b53eb8fce4
Saroj Karki <karki.saroj28@gmail.com>
2020-10-13 14:46:07 +0545

    Docstring added

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+
+def main():
+	''' Main function of the script '''
+
+	img = cv2.imread('task1.png')
+	low_value = (68, 72, 68)
+	high_value = (68, 72, 68)
+	img_final = cv2.inRange(img, low_value, high_value)
+	cv2.imshow('output', img_final)
+	cv2.waitKey(0)
+
+if __name__ == '__main__':
+	main()
