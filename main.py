4dc0733935cc7bcab2adfb26116b5c6ba669bb17
saroj-karki <karki.saroj28@gmail.com>
2020-10-13 13:17:15 +0545

    Update and rename task1.py to main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import cv2
 
 def main():
 	img = cv2.imread("task1.png")
-	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 	low_value = (68, 72, 68)
 	high_value = (68, 72, 68)
 
-	img_final = cv2.inRange(hsv_img, low_value, high_value)
+	img_final = cv2.inRange(img, low_value, high_value)
 	cv2.imshow("output", img_final)
 	cv2.waitKey(0)
 
