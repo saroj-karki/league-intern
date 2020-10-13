7821c5fc3f6936aef5f60f7ca06ee04240c89a39
saroj-karki <karki.saroj28@gmail.com>
2020-10-13 12:12:04 +0545

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,12 @@
 import cv2
 
 img = cv2.imread("task1.png")
+hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 # cv2.imshow("output", img)
 low_value = (68, 72, 68)
 high_value = (68, 72, 68)
 
-imgfinal = cv2.inRange(low_value, high_value)
+imgfinal = cv2.inRange(hsv_img, low_value, high_value)
 cv2.imshow("output", imgfinal)
 cv2.waitKey(0)
